bfd432fe3371f962e215620ee23de519aafd3d2a
Duy <100437122+duydepzai567@users.noreply.github.com>
2026-02-24 20:12:08 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Toaster } from "sonner";
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Toaster />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="space-y-2 text-center">
+      <h1 className="text-4xl font-bold text-transparent bg-primary bg-clip-text">
+        Todo List App
+      </h1>
+
+      <p className="text-muted-foreground">
+        Không có việc gì khó, khó quá thì mình đi ngủ 💪
+      </p>
+    </div>
+  );
+};
